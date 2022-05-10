94d1921efbe2ac7fe2aef9377e24af6b62dc2343
Claire Melville <cmelville@lcs.on.ca>
2022-05-10 09:23:24 -0400

    Initial Commit

---
+++ FidgetAnimationApp/FidgetAnimationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FidgetAnimationAppApp.swift
+//  FidgetAnimationApp
+//
+//  Created by Claire Coding Account on 2022-05-10.
+//
+
+import SwiftUI
+
+@main
+struct FidgetAnimationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
